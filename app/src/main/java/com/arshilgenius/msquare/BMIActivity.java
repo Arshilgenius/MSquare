0d70644afa1eca2403d14032857a82908bc06676
Arshilgenius <Arshilgenius@gmail.com>
2017-02-11 06:21:23 +0530

    changed the bmi activity logic

--- app/src/main/java/com/arshilgenius/msquare/BMIActivity.java
+++ app/src/main/java/com/arshilgenius/msquare/BMIActivity.java
@@ -2,7 +2,11 @@ package com.arshilgenius.msquare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class BMIActivity extends AppCompatActivity {
 
@@ -11,7 +15,35 @@ public class BMIActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
     }
-    EditText wt = (EditText) findViewById(R.id.wt) ;
-    int wt1 = Integer.parseInt( wt.getText().toString() );
+
+    public void bmiCalculate(View view){
+
+        EditText wt = (EditText) findViewById(R.id.wt) ;
+        double wt1 = Double.parseDouble( wt.getText().toString() );
+        EditText ht = (EditText) findViewById(R.id.ht) ;
+        double ht1 = Double.parseDouble( ht.getText().toString() );
+        double bmi=wt1/(ht1*ht1);
+        if(bmi<16){
+            TextView v = (TextView) findViewById(R.id.bmitv);
+            v.setText("Severly UnderWeight");
+        }
+        else if (bmi < 18.5) {
+
+            TextView v = (TextView) findViewById(R.id.bmitv);
+            v.setText("UnderWeight");        }
+        else if (bmi < 25) {
+            TextView v = (TextView) findViewById(R.id.bmitv);
+            v.setText("Normal");
+
+        } else if (bmi < 30) {
+            TextView v = (TextView) findViewById(R.id.bmitv);
+            v.setText("OverWeight");
+        } else {
+            TextView v = (TextView) findViewById(R.id.bmitv);
+            v.setText("Obese");
+        }
+
+    }
+
 
 }
