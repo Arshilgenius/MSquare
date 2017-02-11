c095e9b054bfaef6103f2c8082d667c9f9462a52
Arshilgenius <Arshilgenius@gmail.com>
2017-02-11 06:27:24 +0530

    changed input type of the virtual bp app

--- app/src/main/java/com/arshilgenius/msquare/virtualbp.java
+++ app/src/main/java/com/arshilgenius/msquare/virtualbp.java
@@ -2,6 +2,7 @@ package com.arshilgenius.msquare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class virtualbp extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class virtualbp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_virtualbp);
+    }
+    public void bpmeasure(View view){
+
     }
 
 }
