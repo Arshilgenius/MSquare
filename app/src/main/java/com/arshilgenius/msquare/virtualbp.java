9324eb73643baff086ba87418ade81e6755160d1
Arshilgenius <Arshilgenius@gmail.com>
2017-02-11 06:29:55 +0530

    changed the bp measure activity

--- app/src/main/java/com/arshilgenius/msquare/virtualbp.java
+++ app/src/main/java/com/arshilgenius/msquare/virtualbp.java
@@ -3,6 +3,7 @@ package com.arshilgenius.msquare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class virtualbp extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class virtualbp extends AppCompatActivity {
         setContentView(R.layout.activity_virtualbp);
     }
     public void bpmeasure(View view){
+         EditText e = (EditText) findViewById(R.id.up);
+        int upper = Integer.parseInt(e.getText().toString());
 
     }
 
