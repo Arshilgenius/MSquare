594afe686cc1af6b1b60cfe75893e614ce5d1575
Arshilgenius <Arshilgenius@gmail.com>
2017-03-15 22:35:20 +0530

    added admob

--- app/src/main/java/com/arshilgenius/msquare/MainActivity.java
+++ app/src/main/java/com/arshilgenius/msquare/MainActivity.java
@@ -1,16 +1,22 @@
 package com.arshilgenius.msquare;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
     public void order(View view){
         Intent intent= new Intent(getApplicationContext(),orderActivity.class);
@@ -32,4 +38,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent5= new Intent(getApplicationContext(),virtualbp.class);
         startActivity(intent5);}
 
+
 }
+
+
